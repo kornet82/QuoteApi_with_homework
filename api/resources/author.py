d5f19f688055946fdb38de3b97486a122a22ecaf
kornet82 <azarovp@gmail.com>
2022-06-11 15:45:10 +0300

    add surname

--- api/resources/author.py
+++ api/resources/author.py
@@ -20,20 +20,23 @@ class AuthorResource(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("name", required=True)
+        parser.add_argument("surname", required=True)
         author_data = parser.parse_args()
-        author = AuthorModel(author_data["name"])
+        author = AuthorModel(**author_data)
         db.session.add(author)
         db.session.commit()
         return author_schema.dump(author), 201
 
     def put(self, author_id):
         parser = reqparse.RequestParser()
-        parser.add_argument("name", required=True)
+        parser.add_argument("name")
+        parser.add_argument("surname")
         author_data = parser.parse_args()
         author = AuthorModel.query.get(author_id)
         if author is None:
             return {"Error": f"Author id={author_id} not found"}, 404
-        author.name = author_data["name"]
+        for key,value in author_data.items():
+            setattr(author, key, value)
         db.session.commit()
         return author_schema.dump(author), 200
 
